f0c4e24269f320314644f27c2b0799094934ddea
yoonwoojae <dbsdnwp1004@naver.com>
2020-05-28 16:30:09 +0900

    firebase_database crud example

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -7,11 +11,81 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  List<dynamic> users = [];
+
+  @override
+  void initState() {
+    getUsers();
+    //createUser();
+    updateUser();
+    //deleteUser();
+    super.initState();
+  }
+
+  createUser() async {
+    await usersRef.add({
+      "username": "Jeff",
+      "postsCount": 0,
+      "isAdmin": false,
+    });
+  }
+
+  updateUser() async {
+    /*await usersRef
+        .document("-M8P8Wz8UWAkodczonxV")
+        .updateData({"postsCount": 1});*/
+
+    final doc = await usersRef.document("-M8P8Wz8UWAkodczonxV").get();
+
+    if (doc.exists) {
+      doc.reference.updateData({"postsCount": 1});
+    }
+  }
+
+  deleteUser() async {
+    final doc = await usersRef.document("-M8P8Wz8UWAkodczonxV").get();
+
+    if (doc.exists) {
+      doc.reference.delete();
+    }
+  }
+
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef.getDocuments();
+
+    setState(() {
+      users = snapshot.documents;
+    });
+    /*snapshot.documents.forEach((DocumentSnapshot doc) {
+
+
+    });*/
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: Text('Timeline'),
+      body: Container(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: usersRef.snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return circularProgress();
+            }
+
+            final List<Text> children = snapshot.data.documents
+                .map((doc) => Text(doc['username']))
+                .toList();
+
+            return Container(
+              child: ListView(
+                children: children,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
